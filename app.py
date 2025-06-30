d9fd10eb7838d15564c1e45183688c00f6398323
workonlly <workonlly335@gmail.com>
2025-06-30 07:53:53 +0530

    4th

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from werkzeug.utils import secure_filename
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Allow requests from any origin (frontend on Vercel)
+CORS(app, origins=["https://www.scrapotech.com"])
+
 
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
