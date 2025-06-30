9444c0159bbd9cbc79bcc758323b2696481fb0c8
workonlly <workonlly335@gmail.com>
2025-06-30 07:14:55 +0530

    2nd commit

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def scrape():
         return jsonify({'error': 'Unexpected error', 'details': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
